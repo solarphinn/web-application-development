0d191e6eadde192d10d90129b3094b4060845f49
Bobby St. Jacques, Jr <mobappdevguy@gmail.com>
2017-09-12 21:12:42 -0400

    Updated to prompt for last, first, super name before entering into the database and then listing all entries at the end. Also now uses a prepared statement.

--- src/sqldemo.py
+++ src/sqldemo.py
@@ -9,9 +9,14 @@ cursor.execute('DROP TABLE IF EXISTS superheroes')
 
 cursor.execute('CREATE TABLE IF NOT EXISTS superheroes(last_name varchar(100),first_name varchar(100),hero_name varchar(100))')
 
-cursor.execute('INSERT INTO superheroes VALUES("Parker", "Peter","Spider-Man")')
-cursor.execute('INSERT INTO superheroes values("Wayne", "Bruce","Batman")')
-cursor.execute('INSERT INTO superheroes values("Curry", "Arthur", "Aquaman")')
+go = "y"
+while go == "y":
+  last = raw_input("Enter last name: ")
+  first = raw_input("Enter first name: ")
+  super = raw_input("Enter super hero name: ")
+  cursor.execute('INSERT INTO superheroes VALUES(?,?,?)',(last,first,super))
+  go = raw_input("Enter 'y' to continue: ")
+
 
 rows = cursor.execute('SELECT * FROM superheroes')
 
