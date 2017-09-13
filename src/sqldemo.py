5c373aed5f48fe7743680e629c6245e293baacff
Bobby St. Jacques, Jr <mobappdevguy@gmail.com>
2017-09-12 20:56:02 -0400

    Fixed the bug.

--- src/sqldemo.py
+++ src/sqldemo.py
@@ -9,13 +9,15 @@ cursor.execute('DROP TABLE IF EXISTS superheroes')
 
 cursor.execute('CREATE TABLE IF NOT EXISTS superheroes(last_name varchar(100),first_name varchar(100),hero_name varchar(100))')
 
-
 cursor.execute('INSERT INTO superheroes VALUES("Parker", "Peter","Spider-Man")')
 cursor.execute('INSERT INTO superheroes values("Wayne", "Bruce","Batman")')
 cursor.execute('INSERT INTO superheroes values("Curry", "Arthur", "Aquaman")')
 
+rows = cursor.execute('SELECT * FROM superheroes')
+
+for row in rows:
+  print row
+  print ''
+
 conn.commit()
 conn.close()
-
-
-
