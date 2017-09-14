e2a5d4db2ea0ea1323152dbfcf51eaafa3bdeaf8
Bobby St. Jacques, Jr <mobappdevguy@gmail.com>
2017-09-14 18:13:37 -0400

    Lecture 05 code.

---
+++ www/cgi-bin/lecture05-simple-cookie.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+print 'Content-Type: text/html'
+print 'Set-Cookie: chocolate_chips=delicious;raisin_cookies=terrible'
+print ''
+
+print '''<html>
+  <head><title>Simple Cookie!</title></head>
+  <body>Cookies sent!</body>
+</html>'''
